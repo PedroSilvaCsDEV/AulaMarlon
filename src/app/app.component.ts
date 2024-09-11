a550dbeb8677b964b030d24273b12b8a43c72247
Pedro Silva <pedrolucassantos5280@gmail.com>
2024-09-11 11:29:41 -0300

    Aula do dia 11 de setembro de 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // atributos -> variáveis
+  // metódos -> funções
+  
+  // Interpolação
   title = 'aulaMarlon';
+  nome:string = 'Marlon';
+  idade = 35;
+  url = "https://www.pensamentoverde.com.br/wp-content/uploads/2018/02/Depositphotos_16902961_original_a393978551.jpg"
+  // Property Binding
+  cor ='3px 3px 3px rgb(100,0,120)'
+  constructor(){
+    
+  }
+  imprime(){
+    return 'Oi';
+  }
+  corTexto = ''
+  muda(){
+    let r = Math.random()*255;
+    let g = Math.random()*255;
+    let b = Math.random()*255;
+    this.corTexto = `rgb(${r},${g},${b})`
+  }
+  texto = ""
+  escreve(value:string){
+    this.texto=value;
+  }
+  tema = 'light'
+  mudaTema(value:string){
+    console.log(value);
+    this.tema = value;
+  }
 }
